576f2dac99345707f9c38a5c7561e7d632a41736
Paulo Fava <89317494+pjfava@users.noreply.github.com>
2025-06-01 21:45:55 -0300

    Update database.py

--- Backend/app/database.py
+++ Backend/app/database.py
@@ -2,9 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-#SQLALCHEMY_DATABASE_URL = "mysql+pymysql://usuario:senha@localhost/gerenciador"
-
-SQLALCHEMY_DATABASE_URL = "sqlite+pysqlite:///./vulnerabilidades.db"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://usuario:senha@localhost/gerenciador"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
